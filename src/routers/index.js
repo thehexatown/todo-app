b37568583bc7247282f0e3d362cc208cb2367741
Muhammad Ali <muhammad797@hotmail.com>
2020-02-09 20:04:43 +0500

    Initial Commit

---
+++ src/routers/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from '../screens/Home';
+
+const Stack = createStackNavigator();
+
+const Router = () => (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName={'Home'}>
+      <Stack.Screen name={'Home'} component={Home} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default Router;
